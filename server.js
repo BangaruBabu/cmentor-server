2c8e44744da64da9c354ced936d5ad8020ac6f48
Anusha-Kopparapu <anusha.k5@tcs.com>
2018-06-17 18:42:14 +0530

    storytwo

--- server.js
+++ server.js
@@ -74,4 +74,5 @@ app.post('/api/v1/institute', function (req, res) {
 // port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
 app.listen(3000, function () {
     console.log('Node server  is running on port 3000 to expose RESTAPIS');
+    console.log('anusha');
 });
